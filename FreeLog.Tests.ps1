558e0a003b782657ee65d721ad82ad831834370c
Michael Free <michael.d.free@gmail.com>
2024-09-13 23:52:02 -0400

    Planning init tests

--- FreeLog.Tests.ps1
+++ FreeLog.Tests.ps1
@@ -1,15 +1,33 @@
 # FreeLog.Tests.ps1
 Import-Module .\FreeLog.psm1
 
-Describe "FreeLog Tests" {
+Describe "FreeLog Initializing Tests" {
   BeforeEach {
     $testFilePath = [System.IO.Path]::GetTempFileName()
     Register-EngineEvent PowerShell.Exiting -SupportEvent -Action { Remove-Item -Path $testFilePath -ErrorAction SilentlyContinue }
     }
-    # Initialization Tests
-    It "" {}
-    It "" {}
-    It "" {}
-    It "" {}
-    It "" {}
+    It "should throw an error if LogFilePath is null" {
+    
+    }
+    It "should throw an error if LogFilePath is empty" {
+    
+    }
+    It "should throw an error if LogFilePath directory not exist" {
+    
+    }
+    It "should throw an error if LogFilePath directory not writeable" {
+    
+    }
+    It "should not throw an error if LogFilePath directory writeable" {
+    
+    }
+    It "should create logFile if LogFilePath directory exists" {
+    
+    }
+    It "should add content to Logfile once created with CREATE word" {
+    
+    }
+    It "should append to a prexisting logfile and have a second CREATE word" {
+    
+    }
 }
