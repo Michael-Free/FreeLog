aadf744439485e3199fe3320ca8f5f2e086afcf3
Michael Free <michael.d.free@gmail.com>
2024-09-13 23:41:50 -0400

    Planning class init tests

--- FreeLog.Tests.ps1
+++ FreeLog.Tests.ps1
@@ -2,5 +2,14 @@
 Import-Module .\FreeLog.psm1
 
 Describe "FreeLog Tests" {
-  
+  BeforeEach {
+    $testFilePath = [System.IO.Path]::GetTempFileName()
+    Register-EngineEvent PowerShell.Exiting -SupportEvent -Action { Remove-Item -Path $testFilePath -ErrorAction SilentlyContinue }
+    }
+    # Initialization Tests
+    It "" {}
+    It "" {}
+    It "" {}
+    It "" {}
+    It "" {}
 }
