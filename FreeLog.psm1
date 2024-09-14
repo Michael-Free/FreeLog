f3828a6e03b0c2d353518b70f79c89e0fd894a1f
Michael Free <michael.d.free@gmail.com>
2024-09-13 21:25:54 -0400

    Adding in check for log file

--- FreeLog.psm1
+++ FreeLog.psm1
@@ -1,1 +1,13 @@
-Class FreeLog ()
+class FreeLog (
+  [string]$LogFilePath
+  ([string]$logFilePath) {
+    $this.LogFilePath = $logFilePath
+    $this:EnsureLogFileExists()
+  }
+
+  [void]EnsureLogFileExists() {
+    if (-not (Test-Path -Path $this.LogFilePath)) {
+      # Add Try/Catch Here
+      New-Item -Path $this.LogFilePath -ItemType File -Force
+    }
+)
